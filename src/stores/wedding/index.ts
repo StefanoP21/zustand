8ac8604082cc43f58721697cc488e5c1275b6a67
Stefano <stefanop21@outlook.es>
2024-05-25 13:57:30 -0500

    new: dateSlice

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -3,12 +3,14 @@ import { devtools } from 'zustand/middleware';
 
 import { CreatePersonSlice, PersonSlice } from './person.slice';
 import { CreateGuestSlice, GuestSlice } from './guest.slice';
+import { CreateDateSlice, DateSlice } from './date.slice';
 
-type SharedState = PersonSlice & GuestSlice;
+type SharedState = PersonSlice & GuestSlice & DateSlice;
 
 export const useWeddingBoundStore = create<SharedState>()(
   devtools((...a) => ({
     ...CreatePersonSlice(...a),
     ...CreateGuestSlice(...a),
+    ...CreateDateSlice(...a),
   }))
 );
