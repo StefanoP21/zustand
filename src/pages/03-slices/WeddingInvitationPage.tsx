7362d7367af9cbb1006fdda6506ab562aa49a03b
Stefano <stefanop21@outlook.es>
2024-05-25 14:00:04 -0500

    update: mostrar fecha y hora del store

--- src/pages/03-slices/WeddingInvitationPage.tsx
+++ src/pages/03-slices/WeddingInvitationPage.tsx
@@ -10,6 +10,10 @@ export const WeddingInvitationPage = () => {
   const guestCount = useWeddingBoundStore((state) => state.guestCount);
   const setGuestCount = useWeddingBoundStore((state) => state.setGuestCount);
 
+  const eventDate = useWeddingBoundStore((state) => state.eventDate);
+  const eventYYYYMMDD = useWeddingBoundStore((state) => state.eventYYYYMMDD);
+  const eventHHMM = useWeddingBoundStore((state) => state.eventHHMM);
+
   const handleGuestInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (+e.target.value >= 0) {
       setGuestCount(+e.target.value);
@@ -79,7 +83,12 @@ export const WeddingInvitationPage = () => {
                   <label className="mb-3 block text-base font-medium text-[#07074D]">
                     Fecha de evento
                   </label>
-                  <input type="date" name="eventDate" id="eventDate" />
+                  <input
+                    type="date"
+                    name="eventDate"
+                    id="eventDate"
+                    value={eventYYYYMMDD()}
+                  />
                 </div>
               </div>
               <div className="w-full px-3 sm:w-1/2">
@@ -87,7 +96,12 @@ export const WeddingInvitationPage = () => {
                   <label className="mb-3 block text-base font-medium text-[#07074D]">
                     Hora del evento
                   </label>
-                  <input type="time" name="eventTime" id="eventTime" />
+                  <input
+                    type="time"
+                    name="eventTime"
+                    id="eventTime"
+                    value={eventHHMM()}
+                  />
                 </div>
               </div>
             </div>
