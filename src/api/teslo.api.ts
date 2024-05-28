d01ca8770b3b62babaa4ac8a9930c7a4a2e24a3f
Stefano <stefanop21@outlook.es>
2024-05-27 20:11:05 -0500

    update: check status e interceptores en axios

--- src/api/teslo.api.ts
+++ src/api/teslo.api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import { useAuthStore } from '../stores';
 
 export const tesloApi = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+// interceptors
+tesloApi.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
